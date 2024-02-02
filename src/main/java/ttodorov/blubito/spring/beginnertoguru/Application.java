c8f2ff4207f73d98ecc500cbd1484785bf05f4ec
Todor Todorov <t.todorov@blubito.com>
2024-02-02 14:33:46 +0200

    Added Author and Book pojos with many-to-many relationship

---
+++ src/main/java/ttodorov/blubito/spring/beginnertoguru/Application.java
@@ -0,0 +1,13 @@
+package ttodorov.blubito.spring.beginnertoguru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/ttodorov/blubito/spring/beginnertoguru/ApplicationTests.java
@@ -0,0 +1,13 @@
+package ttodorov.blubito.spring.beginnertoguru;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
